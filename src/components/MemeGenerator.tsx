5c83a29b1108c5bb93921c03224a38cc32ca5dfb
Carlos Ferrer <cferrer33@gmail.com>
2024-04-07 15:49:57 -0500

    build

--- src/components/MemeGenerator.tsx
+++ src/components/MemeGenerator.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { useGetData } from "../custom-hooks/FetchData";
 import html2canvas from "html2canvas";
 
